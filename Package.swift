b19a4cb8a7245c6cee56cee69bebd47a01e27e45
Phillip Avelar <36336702+pavelar@users.noreply.github.com>
2023-12-09 12:57:30 -0600

    fix and simplify bundle resource path #481

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
     name: "ImagePicker",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
